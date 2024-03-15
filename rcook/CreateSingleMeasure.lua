6e6fe50fd34a098bac983aecc152f53085d92f97
Richard Cook <rcook@rcook.org>
2024-03-14 20:33:47 -0700

    Improve error handling

--- rcook/CreateSingleMeasure.lua
+++ rcook/CreateSingleMeasure.lua
@@ -2,9 +2,14 @@ local function print(param)
   reaper.ShowConsoleMsg("[" .. tostring(reaper.time_precise()) .. "] " .. tostring(param) .. "\n")
 end
 
-local function error(param)
+local function userError(param)
   local s = tostring(param)
-  print("[ERROR] " .. s)
+  reaper.ShowMessageBox(s, "CreateSingleMeasure", 0)
+end
+
+local function fatalError(param)
+  local s = tostring(param)
+  reaper.ShowMessageBox("Error: " .. s, "CreateSingleMeasure", 0)
   reaper.ReaScriptError("!" .. s)
 end
 
@@ -47,7 +52,7 @@ local function createSingleMeasure(
   local newMeasureStartTime, newMeasureEndTime = reaper.GetSet_LoopTimeRange2(projectId, false, false, 0, 0, false)
   local newMeasureLength = newMeasureEndTime - newMeasureStartTime
   if newMeasureLength == 0 then
-    error("Invalid selection (empty)")
+    fatalError("Invalid time range selection")
   end
 
   local newMeasureStartQn = reaper.TimeMap2_timeToQN(projectId, newMeasureStartTime)
@@ -59,7 +64,7 @@ local function createSingleMeasure(
   local leadingMeasureStartTime, _, _, originalTimeSigNum, originalTimeSigDenom, originalTempo = reaper.TimeMap_GetMeasureInfo(projectId, measureNumber - 1)
   local leadingMeasureLength = newMeasureStartTime - leadingMeasureStartTime
   if leadingMeasureLength == 0 then
-    error("Invalid selection (no leading)")
+    fatalError("Invalid time range selection")
   end
 
   -- Measure immediately after selection
@@ -68,33 +73,33 @@ local function createSingleMeasure(
   local trailingMeasureEndTime, _, _, _, _, _= reaper.TimeMap_GetMeasureInfo(projectId, measureNumber)
   local trailingMeasureLength = trailingMeasureEndTime - newMeasureEndTime
   if trailingMeasureLength == 0 then
-    error("Invalid selection (no trailing)")
+    fatalError("Invalid time range selection")
   end
 
   local leadingMeasureTempo = 240 / leadingMeasureLength / (leadingMeasureBasis / leadingMeasureQns)
   if not dryRun then
     if not reaper.SetTempoTimeSigMarker(projectId, -1, leadingMeasureStartTime, -1, -1, leadingMeasureTempo, leadingMeasureQns, leadingMeasureBasis, 0) then
-      error("SetTempoTimeSigMarker failed")
+      fatalError("SetTempoTimeSigMarker failed")
     end
   end
 
   local newMeasureTempo = 240 / newMeasureLength / (newMeasureBasis / newMeasureQns)
   if not dryRun then
     if not reaper.SetTempoTimeSigMarker(projectId, -1, newMeasureStartTime, -1, -1, newMeasureTempo, newMeasureQns, newMeasureBasis, 0) then
-      error("SetTempoTimeSigMarker failed")
+      fatalError("SetTempoTimeSigMarker failed")
     end
   end
 
   local trailingMeasureTempo = 240 / trailingMeasureLength / (trailingMeasureBasis / trailingMeasureQns)
   if not dryRun then
     if not reaper.SetTempoTimeSigMarker(projectId, -1, newMeasureEndTime, -1, -1, trailingMeasureTempo, trailingMeasureQns, trailingMeasureBasis, 0) then
-      error("SetTempoTimeSigMarker failed")
+      fatalError("SetTempoTimeSigMarker failed")
     end
   end
 
   if not dryRun then
     if not reaper.SetTempoTimeSigMarker(projectId, -1, trailingMeasureEndTime, -1, -1, originalTempo, originalTimeSigNum, originalTimeSigDenom, 0) then
-      error("SetTempoTimeSigMarker failed")
+      fatalError("SetTempoTimeSigMarker failed")
     end
   end
 end
@@ -104,26 +109,49 @@ local function main()
   local leadingMeasureBasis = 4
   local newMeasureBasis = 4
   local trailingMeasureBasis = 4
-  local dryRun = false
+  local dryRun = true
 
   if reaper.SNM_GetIntConfigVarEx(projectId, "itemtimelock", -100) ~= 0 then
-    error("Timebase for items/envelopes/markers must set to \"Time\"")
+    userError("Timebase for items/envelopes/markers must be set to \"Time\"")
+    return false
   end
 
   if reaper.SNM_GetIntConfigVarEx(projectId, "tempoenvtimelock", -100) ~=0 then
-    error("Timebase for tempo/time signature envelope must be set to \"Time\"")
+    userError("Timebase for tempo/time signature envelope must be set to \"Time\"")
+    return false
   end
 
-  local status, leadingMeasureQnsStr, newMeasureQnsStr, trailingMeasureQnsStr = getUserInputs("TITLE", {{"Beats in leading measure", 4}, {"Beats in new measure", 4}, {"Beats in trailing measure", 4}})
+  local newMeasureStartTime, newMeasureEndTime = reaper.GetSet_LoopTimeRange2(projectId, false, false, 0, 0, false)
+  local newMeasureLength = newMeasureEndTime - newMeasureStartTime
+  if newMeasureLength == 0 then
+    userError("Selected time range is empty")
+    return false
+  end
+
+  local markerCount = reaper.CountTempoTimeSigMarkers(projectId)
+  for i = 0, markerCount - 1 do
+    local status, markerTime = reaper.GetTempoTimeSigMarker(projectId, i)
+    if not status then
+      fatalError("GetTempoTimeSigMarker failed")
+    end
+
+    if markerTime >= newMeasureStartTime and markerTime <= newMeasureEndTime then
+      userError("Selected time range already contains one or more tempo/time signature markers")
+      return false
+    end
+  end
+
+  local status, leadingMeasureQnsStr, newMeasureQnsStr, trailingMeasureQnsStr = getUserInputs("CreateSingleMeasure", {{"Beats in leading measure", 4}, {"Beats in new measure", 4}, {"Beats in trailing measure", 4}})
   if not status then
-    return
+    -- Operation cancelled
+    return false
   end
 
   local leadingMeasureQns = tonumber(leadingMeasureQnsStr)
   local newMeasureQns = tonumber(newMeasureQnsStr)
   local trailingMeasureQns = tonumber(trailingMeasureQnsStr)
 
-  createSingleMeasure(
+  if not createSingleMeasure(
     projectId,
     leadingMeasureQns,
     leadingMeasureBasis,
@@ -131,9 +159,12 @@ local function main()
     newMeasureBasis,
     trailingMeasureQns,
     trailingMeasureBasis,
-    dryRun)
+    dryRun) then
+    return false
+  end
 
   reaper.UpdateTimeline()
+  return true
 end
 
 main()
